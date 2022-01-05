dd44ee206d7ee51b0a30923c280908cbd1808ff6
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-01-05 11:19:59 +0600

    databse and cluster updated

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.afbei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rnuoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 async function run() {
   try {
     await client.connect();
-    const database = client.db(`${process.env.DB_USER}`);
+    const database = client.db(`openpms`);
     const projectsCollection = database.collection('projects');
     const tasksCollection = database.collection('tasks');
 
